7c4153bda3e45086d6f4cc3dca20f1da60653b43
Jefferson Justo <20b3010072@untels.edu.pe>
2023-06-22 16:22:31 -0500

    UC3_Parte02_Justo

--- miapp/views.py
+++ miapp/views.py
@@ -7,6 +7,7 @@ def index(request):
     return render(request,'index.html')
 
 
+
 def index(request):
     return render(request,'index.html', {
     'titulo':'Inicio',
@@ -18,5 +19,15 @@ def index(request):
 def index(request):
     return render(request,'index.html', {
     'titulo':'Inicio',
+})
+
+
+def index(request):
+    cursos = ["Introducción a la Programación", "Bases de Datos", "Diseño de Interfaces de Usuario", "Algoritmos y Estructuras de Datos", "Redes de Computadoras", "Inteligencia Artificial", "Desarrollo Web"]
+
+    return render(request,'index.html', {
+'titulo':'Inicio',
+'mensaje':'Proyecto Web Con DJango',
+'cursos': cursos
 })
 
